84f33770846c586ca82b4d531fe9bedceaf18c99
denizzz <4denizzz@gmail.com>
2013-05-22 21:43:23 +0800

    cleanup

--- protobuf.d
+++ protobuf.d
@@ -3,39 +3,6 @@ module protobuf;
 import std.exception;
 import std.stdio;
 
-string example = q"EOS
-// See README.txt for information and build instructions.
-
-package tutorial;
-
-option java_package = "com.example.tutorial";
-option java_outer_classname = "AddressBookProtos";
-
-message Person {
-  required string name = 1;
-  required int32 id = 2;        // Unique ID number for this person.
-  optional string email = 3;
-
-  enum PhoneType {
-    MOBILE = 0;
-    HOME = 1;
-    WORK = 2;
-  }
-
-  message PhoneNumber {
-    required string number = 1;
-    optional PhoneType type = 2 [default = HOME];
-  }
-
-  repeated PhoneNumber phone = 4;
-}
-
-// Our address book file is just one of these.
-message AddressBook {
-  repeated Person person = 1;
-}
-EOS";
-
 enum WireType : ubyte {
     VARINT           = 0,
     FIXED64          = 1,
@@ -187,23 +154,7 @@ unittest
     assert( unpackString!char( &d[0], next ) == "testing" );
 }
 
-
-int Base128Decode( ubyte* a )
-{
-    
-    writeln( *a );
-    return a[0];
-}
-unittest
-{
-    uint t1 = 0b_00000001;
-//    writeln( Base128Decode( &t1 ) );
-    
-    uint t2[2] = [ 0b_10101100, 0b_00000010 ];
-//    writeln( Base128Decode( &t2[0] ) );
-}
   
 void main()
 {
-    //writeln( example );
 }
