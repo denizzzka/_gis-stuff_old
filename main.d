dd7f24e2f1b8213a0a7f1b4d8b2d7cb300288ac8
denizzz <4denizzz@gmail.com>
2013-06-07 07:26:45 +0800

    blob reading with decompression

--- main.d
+++ main.d
@@ -5,8 +5,38 @@ import osmproto.osmformat;
 import std.stdio;
 import std.string;
 import std.getopt;
-//import std.mmfile;
+import std.exception;
 import std.bitmanip: bigEndianToNative;
+import std.zlib;
+
+
+ubyte[] readBlob( ref File f )
+{
+    ubyte[4] bs = f.rawRead( new ubyte[4] );
+    
+    auto BlobHeader_size = bigEndianToNative!uint( bs );
+    enforce( BlobHeader_size > 0 );
+    
+    auto bhc = f.rawRead( new ubyte[BlobHeader_size] );
+    auto bh = BlobHeader( bhc );
+    enforce( bh.type == "OSMHeader" );
+    
+    auto bc = f.rawRead( new ubyte[bh.datasize] );
+    auto b = Blob( bc );
+    
+    if( b.raw_size.isNull )
+    {
+        debug(osmpbf) writeln( "raw block, size=", b.raw.length );
+        return b.raw;
+    }
+    else
+    {
+        debug(osmpbf) writeln( "zlib compressed block, size=", b.raw_size );
+        enforce( !b.zlib_data.isNull );
+        
+        return cast(ubyte[]) uncompress( b.zlib_data, b.raw_size );
+    }
+}
 
 
 void main( string[] args )
@@ -28,11 +58,5 @@ void main( string[] args )
     log("Open file "~filename);
     auto f = File(filename);
     
-    ubyte[4] bs = f.rawRead( new ubyte[4] );
-    
-    auto BlobHeader_size = bigEndianToNative!uint( bs );
-    log(BlobHeader_size);
-    
-    auto bhc = f.rawRead( new ubyte[BlobHeader_size] );
-    auto bh = BlobHeader( bhc );
+    writeln( readBlob( f ).length );
 }
