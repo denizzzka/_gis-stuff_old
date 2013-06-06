62107638a52292a71a8c189bf1f9fed9fe75604f
denizzz <4denizzz@gmail.com>
2013-06-06 11:52:12 +0800

    compiles

--- protobuf/plugin.cc
+++ protobuf/plugin.cc
@@ -2,14 +2,24 @@
 #include <google/protobuf/compiler/plugin.h>
 
 using namespace google::protobuf;
+using namespace google::protobuf::compiler;
+using namespace google::protobuf::internal;
 
 class DCodeGenerator : public compiler::CodeGenerator
 {
-    virtual bool Generate(const FileDescriptor*, const string&, compiler::GeneratorContext*, std::string*) const
-    {
-        return 1;
-    }
+    virtual bool Generate(const FileDescriptor* file,
+                          const string& parameter,
+                          compiler::GeneratorContext* context,
+                          std::string* error) const;
 };
+
+bool DCodeGenerator::Generate(const FileDescriptor* file,
+                              const string& parameter,
+                              compiler::GeneratorContext* context,
+                              std::string* error) const
+{
+    return false;
+}
 
 int main(int argc, char* argv[])
 {
