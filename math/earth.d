487f5bed6b52d733178e57118861fa19b4715ad7
denizzz <4denizzz@gmail.com>
2013-06-09 12:17:17 +0800

    clearing

--- math/earth.d
+++ math/earth.d
@@ -1,8 +1,7 @@
 import std.math;
-import std.conv;
 
 
-real degree2radian( T )( T val )
+auto degree2radian( T )( T val )
 {
     return val * (PI / 180);
 }
@@ -13,29 +12,12 @@ unittest
     assert( degree2radian(360) == PI * 2 );
 }
 
-struct WGS84
+auto lon2mercator( Datum, T )( T longitude )
 {
-    // major (transverse) radius at the equator in meters
-    static immutable real a = 6378137;
-    
-    //Inverse flattening
-    static immutable inv_flattening = 298.257_223_563;
-    
-    // polar semi-minor (conjugate) radius b in meters
-    static immutable real b = a * (1.0 - 1.0/inv_flattening);
-    unittest
-    {
-        // comparison with the value from WGS84 spec
-        assert( abs(b - 6356752.314245) < 0.000_001 );
-    }
+    return Datum.a * degree2radian( longitude );
 }
 
-real lon2mercatorX( Datum, T )( T longitude )
-{
-    return Datum.a * longitude;
-}
-
-auto lat2mercatorY( Datum, T )( T latitude )
+auto lat2mercator( Datum, T )( T latitude )
 {
     alias Datum D;
     
@@ -48,7 +30,26 @@ auto lat2mercatorY( Datum, T )( T latitude )
     return D.a * log( e1 * e2 );
 }
 
+struct WGS84
+{
+    // Major (transverse) radius at the equator in meters
+    static immutable real a = 6378137;
+    
+    // Inverse flattening
+    static immutable inv_flattening = 298.257_223_563;
+    
+    // Polar semi-minor (conjugate) radius b in meters
+    static immutable real b = a * (1.0 - 1.0/inv_flattening);
+    unittest
+    {
+        // Comparison with the value from WGS84 spec
+        assert( abs(b - 6356752.314245) < 0.000_001 );
+    }
+}
+
 unittest
 {
-    assert( abs( lat2mercatorY!WGS84( 55.751667 ) - 7473789.46 ) < 0.01  );
+    // Latitude and longitude of Moscow 
+    assert( abs( lat2mercator!WGS84( 55.751667 ) - 7473789.46 ) < 0.01 );
+    assert( abs( lon2mercator!WGS84( 37.617778 ) - 4187591.89 ) < 0.01 );
 }
