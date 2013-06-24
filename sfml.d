da8cd03c24205ef2c29f277e499f8b0ba43fcbcc
denizzz <4denizzz@gmail.com>
2013-06-25 06:10:46 +0800

    reformatting

--- sfml.d
+++ sfml.d
@@ -2,42 +2,40 @@ module sfml;
 
 import sf.graphics;
 
-import std.string;
 import std.exception;
 
 
 void init()
 {
-    sfVideoMode vmode = { 800, 600 };
-    sfContextSettings settings = { 24 };
+    sfVideoMode vmode = { width: 640, height: 480, bitsPerPixel: 24 };
+    sfContextSettings settings = { depthBits: 24 };
     
-	auto screen = new RenderWindow(
-        vmode,
-        "Game",
-        sfDefaultStyle,
-        &settings
+    auto screen = new RenderWindow(
+	vmode,
+	"Game",
+	sfDefaultStyle,
+	settings
     );
-
-	auto font = new Font("arial.ttf");
-	auto text = new Text();
-	text.font = font;
-	text.string = "Hello world";
-	text.color = sfBlue;
-	text.position = sfVector2f(10,10);
-
-	sfEvent ev;
-
-	while(screen.isOpen)
-	{	
-		while(screen.pollEvent(&ev))
-		{
-			if(ev.type == sfEvtClosed)
-				screen.close();
-		}
-
-		screen.clear(sfBlack);
-		screen.draw(text, null);			
-		screen.display();
-
+    
+    auto font = new Font("arial.ttf");
+    auto text = new Text();
+    text.font = font;
+    text.string = "Hello world";
+    text.color = sfBlue;
+    text.position = sfVector2f(10,10);
+    
+    sfEvent ev;
+    
+    while(screen.isOpen)
+    {
+	while(screen.pollEvent(&ev))
+	{
+	    if(ev.type == sfEvtClosed)
+	    screen.close();
 	}
+	
+	screen.clear(sfBlack);
+	screen.draw(text, null);			
+	screen.display();
+    }
 }
