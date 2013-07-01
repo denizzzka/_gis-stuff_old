a43ebd67f02feeeb81bbab6b6757d5056cdb1a29
denizzz <4denizzz@gmail.com>
2013-07-02 06:22:10 +0800

    backup

--- main.d
+++ main.d
@@ -1,5 +1,6 @@
 module main;
 
+import math.geometry;
 import osm: getMap;
 import map;
 import scene;
@@ -18,7 +19,11 @@ void main( string[] args )
     );
     
     auto window = new sfml.Window;
-    auto scene = new Scene( getMap( args[1..$], verbose ) );
+    
+    Vector2D!size_t window_size;
+    window_size = window.window.size;
+    
+    auto scene = new Scene( getMap( args[1..$], verbose ), window_size );
     
     /*
     auto map = new Map;
@@ -27,7 +32,9 @@ void main( string[] args )
     map.regions[0].addNode( Node(56,95) );
     map.regions[0].addNode( Node(57,95) );
     */
+    
     writeln( "Map bbox:", scene.map.regions[0].boundary );
+    
     //window.reg = window.new ShowRegion( map.regions[0] );
     //window.mainCycle();
 }
--- scene.d
+++ scene.d
@@ -1,14 +1,22 @@
 module scene;
 
 import map;
+import math.geometry;
 
 
 class Scene
 {
     Map map;
     
-    this( Map m )
+    alias Vector2D!real Vector2r;
+    alias Vector2D!size_t Vector2s;
+    
+    Vector2s window_size;
+    
+    this( Map m, Vector2s pixelsSize )
     {
         map = m;
+        
+        window_size = pixelsSize;
     }
 }
