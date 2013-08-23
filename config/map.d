4ad6ce7455b794d39e1bb6d09e258f1248bc1ee2
denizzz <4denizzz@gmail.com>
2013-08-23 09:54:23 +0800

    roads layers

--- config/map.d
+++ config/map.d
@@ -6,27 +6,33 @@ import sfml: Color;
 struct RoadProperties
 {
     Color color;
+    size_t[] layers;
 }
 
 struct Roads
 {
     RoadProperties HIGHWAY = {
-            color: Color.Green
+            color: Color.Green,
+            layers: [ 4 ]
         };
     
     RoadProperties PRIMARY = {
-            color: Color.White
+            color: Color.White,
+            layers: [ 2 ]
         };
     
     RoadProperties SECONDARY = {
-            color: Color.Yellow
+            color: Color.Yellow,
+            layers: [ 1 ]
         };
     
     RoadProperties OTHER = {
-            color: Color( 0xAA, 0xAA, 0xAA )
+            color: Color( 0xAA, 0xAA, 0xAA ),
+            layers: [ 0 ]
         };
     
     RoadProperties UNSUPPORTED = {
-            color: Color.Magenta
+            color: Color.Magenta,
+            layers: [ 0, 1, 2, 3, 4 ]
         };
 }
