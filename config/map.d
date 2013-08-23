c3eb29bd9edc2e529682df561a8d777844ab59b0
denizzz <4denizzz@gmail.com>
2013-08-23 09:50:32 +0800

    all roads

--- config/map.d
+++ config/map.d
@@ -13,4 +13,20 @@ struct Roads
     RoadProperties HIGHWAY = {
             color: Color.Green
         };
+    
+    RoadProperties PRIMARY = {
+            color: Color.White
+        };
+    
+    RoadProperties SECONDARY = {
+            color: Color.Yellow
+        };
+    
+    RoadProperties OTHER = {
+            color: Color( 0xAA, 0xAA, 0xAA )
+        };
+    
+    RoadProperties UNSUPPORTED = {
+            color: Color.Magenta
+        };
 }
