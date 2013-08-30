8606618b1e1073b610d27f0ff3048f63133b24f0
denizzz <4denizzz@gmail.com>
2013-08-30 19:48:12 +0800

    building color fix

--- config/map.d
+++ config/map.d
@@ -25,78 +25,6 @@ class Polylines
         return properties[ enum_type ];
     }
     
-    static this()
-    {
-        import std.stdio;
-        writeln( members );
-        writeln( typeid( members ) );
-        
-        PolylineProperties rp;
-        
-        // OTHER
-        rp = PolylineProperties(
-                Color( 0x00, 0xAA, 0xAA ),
-                [ 0 ]
-            );
-        properties[0] = rp;
-        
-        // BUILDING
-        rp = PolylineProperties(
-                Color( 0xf7, 0xc3, 0x94 ),
-                [ 0 ]
-            );
-        properties[1] = rp;
-        
-        // BOUNDARY
-        rp = PolylineProperties(
-                Color( 0xAA, 0xAA, 0x00 ),
-                [ 0, 1, 2, 3, 4 ]
-            );
-        properties[2] = rp;
-        
-        // PATH
-        rp = PolylineProperties(
-                Color.Magenta,
-                [ 0, 1, 2, 3, 4 ]
-            );
-        properties[3] = rp;
-        
-        // HIGHWAY
-        rp = PolylineProperties(
-                Color.Green,
-                [ 0, 1, 2, 3, 4 ]
-            );
-        properties[4] = rp;
-        
-        // PRIMARY
-        rp = PolylineProperties(
-                Color.White,
-                [ 0, 1, 2, 3 ]
-            );
-        properties[5] = rp;
-        
-        // SECONDARY
-        rp = PolylineProperties(
-                Color.Yellow,
-                [ 0, 1, 2 ]
-            );
-        properties[6] = rp;
-        
-        // ROAD_OTHER
-        rp = PolylineProperties(
-                Color( 0xAA, 0xAA, 0xAA ),
-                [ 0, 1 ]
-            );
-        properties[7] = rp;
-        
-        // UNSUPPORTED
-        rp = PolylineProperties(
-                Color.Cyan,
-                [ 0, 1 ]
-            );
-        properties[8] = rp;
-    }
-    
     this( string filename )
     {
         string file_content = readText( filename );
@@ -120,12 +48,12 @@ Color getColor( JSONValue v )
 {
     auto color_vals = v.object["color"];
     
-    ubyte colorChan( string name )
+    ubyte chan( string name )
     {
-        return to!ubyte( color_vals["R"].uinteger );
+        return to!ubyte( color_vals[ name ].uinteger );
     }
     
-    return Color( colorChan("R"), colorChan("G"), colorChan("B") );
+    return Color( chan("R"), chan("G"), chan("B") );
 }
 
 size_t[] getSize_tArray( JSONValue v )
