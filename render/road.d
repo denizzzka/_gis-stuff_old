ae59f46acd0fe5287b18b517c5ff3c78cfe625f2
denizzz <4denizzz@gmail.com>
2013-09-09 04:45:11 +0800

    drawCircle

--- render/road.d
+++ render/road.d
@@ -2,12 +2,24 @@ module render.road;
 
 import dsfml.graphics;
 import cat = config.categories;
+import config.map: polylines;
 
 
 mixin template Road()
 {
     void drawRoadBend( WindowCoords coords, cat.Line type )
     {
-        auto sfml_coords = cartesianToSFML( coords );
+        Vector2f sfml_coords; sfml_coords = cartesianToSFML( coords );
+        
+        auto property = &polylines.getProperty( type );
+        
+        auto circle = new CircleShape;
+        
+        circle.radius = property.thickness / 2;
+        circle.outlineThickness = property.outlineThickness;
+        circle.fillColor = property.color;
+        circle.position = sfml_coords;
+        
+        window.draw( circle );
     }
 }
