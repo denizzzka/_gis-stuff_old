c2683b39eb6d148bb2ed67a620ccc1c9b0f38921
denizzz <4denizzz@gmail.com>
2013-09-10 03:28:50 +0800

    undirected graph adding

---
+++ math/graph/undirected.d
@@ -0,0 +1,68 @@
+module math.graph.undirected;
+
+import math.graph.iface;
+
+
+struct NodeDescr { size_t idx; }
+struct EdgeDescr { size_t idx; }
+
+class UndirectedGraph( Point, EdgePayload ) : IGraph!( Point, EdgePayload, NodeDescr, EdgeDescr )
+{
+    struct Edge
+    {
+        NodeDescr from_node;
+        NodeDescr to_node;
+        
+        EdgePayload payload;
+    }
+    
+    struct Node
+    {
+        package Edge[] edges;
+        
+        Point point;
+        
+        EdgeDescr addEdge( Edge edge )
+        {
+            EdgeDescr res = { idx: edges.length };
+            
+            edges ~= edge;
+            
+            return res;
+        }
+    }
+    
+    package Node[] nodes;
+    
+    bool isAvailable( in NodeDescr nd ) const
+    {
+        return nd.idx < nodes.length;
+    }
+    
+    NodeDescr addPoint( Point v )
+    {
+        NodeDescr res = { idx: nodes.length };
+        
+        Node n = { point: v };
+        nodes ~= n;
+        
+        return res;
+    }
+    
+    EdgeDescr addEdge( in ConnectionInfo conn, EdgePayload edgePayload )
+    {
+        Edge e = { to_node: conn.to, payload: edgePayload };
+        
+        return nodes[ conn.from.idx ].addEdge( e );
+    }
+    
+    const (EdgePayload)* getEdgePayload( in NodeDescr node, in EdgeDescr edge ) const
+    {
+        return &nodes[ node.idx ].edges[ edge.idx ].payload;
+    }
+}
+
+unittest
+{
+    auto t = new UndirectedGraph!( float, float );
+}
