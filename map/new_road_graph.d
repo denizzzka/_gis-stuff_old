2afd5deab2ba52f30e7fcaf2c55aa21d714611bd
denizzz <4denizzz@gmail.com>
2013-09-10 08:20:23 +0800

    works, backup

--- map/new_road_graph.d
+++ map/new_road_graph.d
@@ -3,16 +3,15 @@ module map.new_road_graph;
 import map.new_map_graph;
 
 
-struct Point
+struct RoadPoint
 {
-    map.new_map_graph.GraphPoint point;
-    
+    GraphPoint point;
     alias point this;
     
     byte level = 0;
 }
 
-class RoadGraph : MapGraph!( Point )
+class RoadGraph : MapGraph!( RoadPoint )
 {
 }
 
