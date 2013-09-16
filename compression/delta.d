d02835a6a3f3d73515674189f6e2ed61d52e2bc6
denizzz <4denizzz@gmail.com>
2013-09-16 12:03:54 +0800

    deltacode works

--- compression/delta.d
+++ compression/delta.d
@@ -1,18 +1,30 @@
 module compression.delta;
 
+import std.math: trunc, lround;
+import std.conv: to;
 
-class DeltaEncoded( T )
+
+class DeltaEncoded( T, size_t keyInterval )
 {
     T[] encoded;
+    
+    private
+    size_t findKey( in size_t idx )
+    {
+        return idx / keyInterval * keyInterval;
+    }
     
     void opOpAssign( string op )( T v )
     if( op == "~" )
     {
-        if( !encoded )
+        // first element of frame
+        if( encoded.length % keyInterval == 0 )
             encoded ~= v;
         else
         {
-            for( auto i = 0; i < encoded.length; i++ )
+            auto key = findKey( encoded.length - 1 );
+            
+            for( auto i = key; i < key+keyInterval && i < encoded.length; i++ )
                 v -= encoded[i];
             
             encoded ~= v;
@@ -27,9 +39,11 @@ class DeltaEncoded( T )
     }
     body
     {
-        T res = encoded[0];
+        auto key = findKey( idx );
         
-        for( auto i = 1; i <= idx; i++ )
+        T res = encoded[key];
+        
+        for( auto i = key+1; i <= idx; i++ )
             res += encoded[i];
         
         return res;
@@ -38,15 +52,28 @@ class DeltaEncoded( T )
 
 unittest
 {
-    alias DeltaEncoded!float D;
+    alias DeltaEncoded!( float, 3 ) D;
     
     D d = new D;
     
+    assert( 2 / 3 == 0 );
+    assert( d.findKey( 2 ) == 0 );
+    
+    d ~= 0.0f;
     d ~= 1.0f;
     d ~= 2.0f;
     d ~= 3.0f;
+    d ~= 4.0f;
+    d ~= 5.0f;
+    d ~= 6.0f;
+    d ~= 7.0f;
     
-    assert( d[0] == 1 );
-    assert( d[1] == 2 );
-    assert( d[2] == 3 );
+    assert( d[0] == 0 );
+    assert( d[1] == 1 );
+    assert( d[2] == 2 );
+    assert( d[3] == 3 );
+    assert( d[4] == 4 );
+    assert( d[5] == 5 );
+    assert( d[6] == 6 );
+    assert( d[7] == 7 );
 }
