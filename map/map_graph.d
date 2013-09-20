203f1cad2edb8c5ed45fbd684d1ba5cbcba20dba
denizzz <4denizzz@gmail.com>
2013-09-20 13:59:51 +0800

    small change

--- map/map_graph.d
+++ map/map_graph.d
@@ -204,7 +204,7 @@ Description[] cutOnCrossings( Description )( Description[] lines )
     alias Description.BBox BBox;
     alias RTreePtrs!( BBox, Description ) DescriptionsTree;
     
-    auto tree = new DescriptionsTree( 4, 1 );
+    auto tree = new DescriptionsTree( 4, 10 );
     
     foreach( ref c; lines )
     {
