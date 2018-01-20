7924b79f2c3fd31eda77922b9a6bb53c358e74c6
Denis Feklushkin <denis.feklushkin@gmail.com>
2018-01-20 17:47:49 +0700

    OSM files .proto moved to 3 version

--- source/pbf/package.d
+++
@@ -1,8 +0,0 @@
-module pbf;
-
-import dproto.dproto;
-
-mixin ProtocolBuffer!"map_objects.proto";
-mixin ProtocolBuffer!"compressed_array.proto";
-mixin ProtocolBuffer!"digraph_compressed.proto";
-mixin ProtocolBuffer!"undirected_graph_compressed.proto";
